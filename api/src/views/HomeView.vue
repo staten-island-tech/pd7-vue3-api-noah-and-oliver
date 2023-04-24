30779edef540bc81dffbcefda5057a35ad850647
LilNoahChan <noahchan305@gmail.com>
2023-04-24 13:13:48 -0400

    2

--- api/src/views/HomeView.vue
+++ api/src/views/HomeView.vue
@@ -4,7 +4,15 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const pokemon = ref('')
+const SAT = ref('')
+async function getSAT() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  let data = await res.json()
+  sat.value = data.results
+}
+onMounted(() =>{
+  getPokemon()
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
