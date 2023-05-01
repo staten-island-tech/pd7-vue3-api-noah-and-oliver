fb07ddb13e3eb80686de839b1295936a0263b804
oliiversion <olivxrkei101@gmail.com>
2023-05-01 12:45:43 -0400

    finally

--- api/src/App.vue
+++ api/src/App.vue
@@ -1,85 +1,68 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th @click="sortBy('dbn')">DBN</th>
+          <th @click="sortBy('school_name')">School Name</th>
+          <th @click="sortBy('num_of_sat_test_takers')">Number of SAT Test Takers</th>
+          <th @click="sortBy('sat_critical_reading_avg_score')">SAT Critical Reading Average Score</th>
+          <th @click="sortBy('sat_math_avg_score')">SAT Math Average Score</th>
+          <th @click="sortBy('sat_writing_avg_score')">SAT Writing Average Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(school, index) in sortedSchools" :key="index">
+          <td>{{ school.dbn }}</td>
+          <td>{{ school.school_name }}</td>
+          <td>{{ school.num_of_sat_test_takers }}</td>
+          <td>{{ school.sat_critical_reading_avg_score }}</td>
+          <td>{{ school.sat_math_avg_score }}</td>
+          <td>{{ school.sat_writing_avg_score }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+export default (await import ('vue')).defineComponent{
+  data() {
+    return {
+      schools: [],
+      sortKey: 'dbn',
+      sortDirection: 1
+    };
+  },
+  async mounted() {
+    const response = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json?$query=SELECT%0A%20%20%60dbn%60%2C%0A%20%20%60school_name%60%2C%0A%20%20%60num_of_sat_test_takers%60%2C%0A%20%20%60sat_critical_reading_avg_score%60%2C%0A%20%20%60sat_math_avg_score%60%2C%0A%20%20%60sat_writing_avg_score%60');
+    const data = await response.json();
+    this.schools = data;
+  },
+  computed: {
+    sortedSchools() {
+      return this.schools.sort((a, b) => {
+        const sortColumnA = a[this.sortKey];
+        const sortColumnB = b[this.sortKey];
+        if (sortColumnA < sortColumnB) {
+          return -1 * this.sortDirection;
+        }
+        if (sortColumnA > sortColumnB) {
+          return 1 * this.sortDirection;
+        }
+        return 0;
+      });
+    }
+  },
+  methods: {
+    sortBy(sortKey) {
+      if (this.sortKey === sortKey) {
+        this.sortDirection *= -1;
+      } else {
+        this.sortKey = sortKey;
+        this.sortDirection = 1;
+      }
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+};
+</script>
---
+++ api/src/views/PersonData.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="cards">
+      <Card
+        v-for="person in persons"
+        :key="person._311sr_number"
+        :borough="person.borough"
+        :property="person.property"
+        :location="person.location"
+        :species="person.species_description"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import Card from '../components/Card.vue'
+  export default(await import ('vue')).defineComponent{
+      data() {
+          return {
+              person: []
+          };
+      },
+      methods: {
+          getAnimal: async function () {
+              let res = await fetch("https://data.cityofnewyork.us/resource/fuhs-xmg2.json");
+              let data = await res.json();
+              data.forEach(person => {
+                  this.persons.push(animal);
+              });
+          }
+      },
+      mounted: async function () {
+          await this.getPerson();
+      },
+      components: { Card }
+  }
+  </script>
+  
+  <style scoped>
+    .cards {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+  </style>
+  
